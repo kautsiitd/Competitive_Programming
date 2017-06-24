2937ee9fab508410303b9496ab5d564e6a2e1fd4
Kautsya Kanu <kautsiitd@gmail.com>
2017-06-25 03:47:47 +0530

    Added solution for marbles problem

---
+++ CodeChef/Practice/Medium/MARBLES.py
@@ -0,0 +1,11 @@
+def nCr(n,r):
+    answer = 1
+    for i in range(r):
+        answer*=(n-i)
+    for i in range(1,r+1):
+        answer/=i
+    return answer
+
+for _ in range(input()):
+    n,k = map(int,raw_input().split())
+    print nCr(n-1,k-1)
