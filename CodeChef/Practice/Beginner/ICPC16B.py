6d8c8586cca451c3d44657720c89590ccfca9a09
Kautsya Kanu <kautsiitd@gmail.com>
2017-07-02 03:09:33 +0530

    Corrected solution for ICPC16B problem as negative integers were also allowed

--- CodeChef/Practice/Beginner/ICPC16B.py
+++ CodeChef/Practice/Beginner/ICPC16B.py
@@ -1,11 +1,16 @@
-for _ in range(input()):
-    input()
-    a = map(int,raw_input().split())
-    nGreater = 0
-    for i in a:
-        if i>1:
-            nGreater += 1
-    if nGreater > 1:
-        print "no"
+n = int(raw_input())
+for i in xrange(n):
+    N = int(raw_input())
+    l1 = map(int, raw_input().split())
+    a = l1.count(0)
+    b = l1.count(1)
+    c = l1.count(-1)
+    sm = N-a-b-c
+    if sm>1:
+        print 'no'
+    elif sm>0 and c>0:
+        print 'no'
+    elif b==0 and c>1:
+        print 'no'
     else:
-        print "yes"
+        print 'yes'
