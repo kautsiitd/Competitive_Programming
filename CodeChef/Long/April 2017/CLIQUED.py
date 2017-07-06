3dcd6d254c4fa5fd18291a416dd1906e52b0bd20
Kautsya Kanu <kautsiitd@gmail.com>
2017-07-07 00:01:38 +0530

    Updated solution for CLIQUED problem

--- CodeChef/Long/April 2017/CLIQUED.py
+++ CodeChef/Long/April 2017/CLIQUED.py
@@ -1,26 +1,16 @@
 import heapq as hq
-
-
 def djik(graph,s,n):
     dist={}
     vis=[False]*(n+2)
     for i in range(1,n+2):
         dist[i]=float('inf')
     dist[s]=0
-    #vis[s]=True
     h=[]
     hq.heappush(h,(0,s))
-    #for i in graph[s]:
-    #    hq.heappush(h,i)
-    #print h
     while not len(h)==0:
-        #print h,
         a=hq.heappop(h)
-        #print a
         if vis[int(a[1])]==True:
-            #pass
             continue
-        ###print a
         u=a[1]
         d=a[0]
         for j in graph[u]:
@@ -32,10 +22,8 @@ def djik(graph,s,n):
         vis[u]=True
     return dist
 
-t= int(raw_input())
-while t:
-    t=t-1
-    n,k,x,m,s=map(int,raw_input().split())
+for _ in range(input()):
+    n,k,x,m,s = map(int,raw_input().split())
     graph={}
     for i in range(1,n+2):
         graph[i]=[]
@@ -47,7 +35,6 @@ while t:
         a,b,c=map(int,raw_input().split())
         graph[a].append((c,b))
         graph[b].append((c,a))
-    #print graph
     d= djik(graph,s,n)
     for i in d:
         if i!=n+1:
