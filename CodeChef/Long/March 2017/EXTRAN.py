d899e52169ba155b72b1c51bd71399fccd675682
Kautsya Kanu <kautsiitd@gmail.com>
2017-07-08 03:43:03 +0530

    Updated solution of EXTRAN with very easy logic

--- CodeChef/Long/March 2017/EXTRAN.py
+++ CodeChef/Long/March 2017/EXTRAN.py
@@ -1,18 +1,12 @@
 for _ in range(input()):
     n = input()
     a = sorted(map(int, raw_input().split()))
-    found = False
-    for i in range(1,n-2):
-        if a[i+1] - a[i] != 1:
-            found = True
-            if a[i+2] - a[i+1] != 1:
-                print a[i+1]
-                break
-            if a[i] - a[i-1] != 1:
+    if a[1] - a[0] != 1:
+        print a[0]
+    elif a[-1] - a[-2] != 1:
+        print a[-1]
+    else:
+        for i in range(n-1):
+            if a[i] == a[i+1]:
                 print a[i]
                 break
-    if not found:
-        if a[1] - a[0] != 1:
-            print a[0]
-        else:
-            print a[-1]
