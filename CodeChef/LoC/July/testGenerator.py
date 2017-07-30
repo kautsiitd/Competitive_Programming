c94eece7fc5ba18ecdf0f0fc9244ed7a852c6974
Kautsya Kanu <kautsiitd@gmail.com>
2017-07-30 13:07:47 +0530

    Addes testGenerator file for CAVECOIN

--- CodeChef/LoC/July/testGenerator.py
+++ CodeChef/LoC/July/testGenerator.py
@@ -1,4 +1,19 @@
-print 10
+import random
+t = 5
+print t
 
-for i in range(10):
-    print i
+for _ in range(t):
+    n = random.randint(1,1000)
+    m = random.randint(1,min(n,100))
+    print n,m
+    upperLimit = 100000
+    for i in range(n):
+        a = random.randint(0,upperLimit)
+        b = random.randint(a,upperLimit)
+        print a,b
+
+    kSize = 60
+    k = "1"
+    for i in range(1,kSize):
+        k += str(random.randint(0,1))
+    print k
