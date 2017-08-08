62dd20e40fce037b4e8aec26cb79ddb8e6b03d63
Kautsya Kanu <kautsiitd@gmail.com>
2017-08-08 20:58:03 +0530

    Added iterative solution for CHEFFA problem

--- CodeChef/Long/August 2017/CHEFFA.py
+++ CodeChef/Long/August 2017/CHEFFA.py
@@ -1,38 +1,37 @@
-import sys
-sys.setrecursionlimit(1000000)
+a = [[1 for i in range(101)] for j in range(101)]
+mod = 1000000007
 
-def f(l):
-    global n,ans
-    newL = l
-    for i in range(1,l):
-        if b[i]>0 and b[i-1]>0:
-            b[i] -= 1
-            b[i-1] -= 1
-            if i == l-1:
-                b.append(1)
-                newL += 1
-            else:
-                b[i+1] += 1
-            if not(" ".join(map(str,b)) in dict):
-                dict[" ".join(map(str,b))] = True
-                ans += 1
-                f(newL)
-            b[i] += 1
-            b[i-1] += 1
-            if i == l-1:
-                b.pop()
-            else:
-                b[i+1] -= 1
+def find(i,j):
+    if i>j:
+        return a[j][j]
+    else:
+        return a[i][j]
+
+for i in range(101):
+    for j in range(1,101):
+        ans = 0
+        for k in range(min(i,j)+1):
+            ans += find(j-k,k)%mod
+        a[i][j] = ans%mod
+
+def findAns(level,firstNum,secondNum):
+    if level == n-2:
+        return a[firstNum][secondNum]
+    elif (level,firstNum,secondNum) in dict:
+        return dict[(level,firstNum,secondNum)]
+    else:
+        ans = 0
+        for k in range(min(firstNum,secondNum)+1):
+            ans += findAns(level+1,secondNum-k,arr[level+2]+k)%mod
+        ans %= mod
+        dict[(level,firstNum,secondNum)] = ans
+        return ans
 
 for _ in range(input()):
-    al = input()
-    a = map(int,raw_input().split())
-    if al == 1:
+    n = input()
+    arr =map(int,raw_input().split())
+    if n == 1:
         print 1
         continue
-    n = 2
-    b = [a[-2],a[-1]]
     dict = {}
-    ans = 1
-    f(n)
-    print ans%1000000007
+    print findAns(0,arr[0],arr[1])
