e3ceada1bdfc08907b70626c679b194b96089b54
Kautsya Kanu <kautsiitd@gmail.com>
2017-08-29 17:22:36 +0530

    Working on ALTSUB problem solution from LoC challenge

---
+++ CodeChef/LoC/August/ALTSUB.py
@@ -0,0 +1,10 @@
+import math
+
+n,Q = map(int,raw_input().split())
+a = [input() for i in range(n)]
+for level in range(int(math.log2(n))+1):
+    for levelElements in range(pow(2,level)):
+        a[]
+
+for _ in range(Q):
+    q = map(int,raw_input().split())
