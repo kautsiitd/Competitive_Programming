0ed22e4bd9e04131f85051e88cfaf255d9bbe98a
Kautsya Kanu <kautsiitd@gmail.com>
2017-09-02 05:43:00 +0530

    Added pascal triangle concept in WEASELT python solution
    Former-commit-id: 29c21dbe0fee5c89d115575913601219ea19f471 [formerly 35f2602e414ed8d871422382b8be7dedb645aa90]
    Former-commit-id: 2bc0877042ee11aef04d60d4ae838d66e5df03ce

--- CodeChef/Long/September 2017/WEASELTX.py
+++ CodeChef/Long/September 2017/WEASELTX.py
@@ -13,21 +13,22 @@ def dfs(current,parent,currentDepth):
             depth[child] = currentDepth+1
             dfs(child,current,currentDepth+1)
 dfs(0,-1,0)
-maxDepth = max(depth)
+
+def numberOfTwosInFactorial(num):
+    temp = 2
+    ans = 0
+    while temp <= num:
+        ans += num/temp
+        temp*=2
+    return ans
 
 queries = [input() for i in range(q)]
-maxDelta = max(queries)
-
-matrix = [[1] for i in range(maxDelta+1)]
-for _ in range(maxDepth):
-    matrix[0].append(1)
-for i in range(1,maxDelta+1):
-    for j in range(1,maxDepth+1):
-        matrix[i].append(matrix[i-1][j]^matrix[i][j-1])
-
 for delta in queries:
     ans = values[0]
     for i in range(1,n):
-        if matrix[delta-1][depth[i]] == 1:
+        k1 = numberOfTwosInFactorial(delta-1+depth[i])
+        k2 = numberOfTwosInFactorial(depth[i])
+        k3 = numberOfTwosInFactorial(delta-1)
+        if k1<=k2+k3:
             ans ^= values[i]
     print ans
