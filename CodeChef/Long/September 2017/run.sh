2dc9bf3ba2c85214172f5930aed13d9a0b716fe9
Kautsya Kanu <kautsiitd@gmail.com>
2017-09-02 05:42:32 +0530

    Include testGenerator and run script files
    Former-commit-id: d0c89f4f878af869e008f72e518fb13b7828a99c [formerly 60081534a17adb12d07289ecc1f7e3797447ae90]
    Former-commit-id: 73e74db51b7bbf9474cbfd9d761541c93b50de3c

---
+++ CodeChef/Long/September 2017/run.sh
@@ -0,0 +1,2 @@
+g++ WEASELTX.cpp -std=c++14
+time ./a.out < test > 1.out
---
+++ CodeChef/Long/September 2017/testGenerator.py
@@ -0,0 +1,10 @@
+import random
+n,q = random.randint(400,500),random.randint(199999,200000)
+print n,q
+for i in range(n-1):
+    print i,i+1
+for _ in range(n):
+    print random.randint(999999999999999990,1000000000000000000),
+print ""
+for _ in range(q):
+    print random.randint(0,500)
