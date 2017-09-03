80f5789ec6cc754c47b928737f88b39a3693cbe2
Kautsya Kanu <kautsiitd@gmail.com>
2017-09-03 10:09:51 +0530

    Added combinatoin in range for solution of SEAFUNC problem
    Former-commit-id: 87d80d673dd58033c5d0f7b55f928ad22ee0ede5 [formerly d4d7a5c41b94205d04b80407dfbb67bbe3226cd0]
    Former-commit-id: de91c30ce5b97ea4d92a649334cda6ef6239983e

--- CodeChef/Long/September 2017/SEAFUNC.py
+++ CodeChef/Long/September 2017/SEAFUNC.py
@@ -1,7 +1,22 @@
 for _ in range(input()):
     n = input()
     a = [[int(i) for i in raw_input()] for _ in range(n)]
-    onePos = [(i+1,j+1) for i in range(n) for j in range(n) if a[i][j] == 1]
-    print len(onePos)
-    for point in onePos:
-        print 0,1,0,1,0,1,point[1],point[0],point[0]
+    ans = []
+    for j in range(n):
+        i = 0
+        while i<n:
+            if a[i][j] == 1:
+                l = i+1
+                while i<n and a[i][j] == 1:
+                    i+=1
+                r = i
+                ans.append((0,1,0,1,0,1,j+1,l,r))
+            else:
+                i+=1
+
+    # printing ans
+    print len(ans)
+    for i in ans:
+        for j in i:
+            print j,
+        print ""
