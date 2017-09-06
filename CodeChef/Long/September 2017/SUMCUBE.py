29b0641c3ed076514a32ff3b4ab73534b50dacae
Kautsya Kanu <kautsiitd@gmail.com>
2017-09-06 14:30:27 +0530

    Added solution for k==3 for SUMCUBE problem
    Former-commit-id: 95af4fc577e79506f13d17fdf231a4c62d315b6f [formerly c91db182384c72cece346349a9d8cb82a89d59d7]
    Former-commit-id: 85f48f0cffe18de0083b34d59882a27d9ce21846

--- CodeChef/Long/September 2017/SUMCUBE.py
+++ CodeChef/Long/September 2017/SUMCUBE.py
@@ -1,4 +1,4 @@
-import collections
+from itertools import combinations
 import sys
 sys.setrecursionlimit(1000000)
 
@@ -6,6 +6,35 @@ mod = 1000000007
 powTwo = [1]
 for _ in range(100005):
     powTwo.append((powTwo[-1]*2)%mod)
+
+def setVariables3():
+    global ThreeEdgeUsingThreeNodes,ThreeEdgeUsingFourNodes,ThreeEdgeUsingFiveNodes,ThreeEdgeUsingSixNodes
+    global ThreeEdgeUsingThreeNodesAns,ThreeEdgeUsingFourNodesAns,ThreeEdgeUsingFiveNodesAns,ThreeEdgeUsingSixNodesAns
+    for triplet in combinations(allEdges,3):
+        elements = []
+        for i in triplet:
+            for j in i:
+                elements.append(j)
+        elements = list(set(elements))
+        l = len(elements)
+        # print triplet,l
+        if l == 3:
+            ThreeEdgeUsingThreeNodes += 1
+        elif l == 4:
+            ThreeEdgeUsingFourNodes += 1
+        elif l == 5:
+            ThreeEdgeUsingFiveNodes += 1
+        else:
+            ThreeEdgeUsingSixNodes += 1
+    # print ThreeEdgeUsingThreeNodes,ThreeEdgeUsingFourNodes,ThreeEdgeUsingFiveNodes,ThreeEdgeUsingSixNodes
+    if v>=3:
+        ThreeEdgeUsingThreeNodesAns = (6*powTwo[v-3]*ThreeEdgeUsingThreeNodes)%mod
+    if v>=4:
+        ThreeEdgeUsingFourNodesAns = (6*powTwo[v-4]*ThreeEdgeUsingFourNodes)%mod
+    if v>=5:
+        ThreeEdgeUsingFiveNodesAns = (6*powTwo[v-5]*ThreeEdgeUsingFiveNodes)%mod
+    if v>=6:
+        ThreeEdgeUsingSixNodesAns = (6*powTwo[v-6]*ThreeEdgeUsingSixNodes)%mod
 
 def customPow(a,b):
     ans = 1
@@ -15,43 +44,59 @@ def customPow(a,b):
         b-=1
     return ans
 
-def dfs(currentVertex,currentEdges):
-    global ans
-    if currentVertex > v:
-        return
-    visited[currentVertex] = True
-    i=1
-    while currentVertex+i <= v and visited[currentVertex+i]:
-        i+=1
-    dfs(currentVertex+i,currentEdges)
-    included[currentVertex] = True
-    newEdges = sum([1 for child in graph[currentVertex] if included[child]])
-    currentEdges += newEdges
-    currentEdges %= mod
-    value = customPow(currentEdges,k)%mod
-    ans += value
-    ans %= mod
-    while currentVertex+i <= v and visited[currentVertex+i]:
-        i+=1
-    dfs(currentVertex+i,currentEdges)
-    visited[currentVertex] = False
-    included[currentVertex] = False
-
 for _ in range(input()):
     v,e,k = map(int,raw_input().split())
     graph = [[] for __ in range(v+1)]
+    edgeFrom = [0 for i in range(v+1)]
+    allEdges = []
     for __ in range(e):
         a,b = map(int,raw_input().split())
         graph[a].append(b)
         graph[b].append(a)
-    if v == 1:
+        edgeFrom[a] += 1
+        edgeFrom[b] += 1
+        allEdges.append([a,b])
+    if v == 1 or e == 0:
         print 0
+    elif v == 2:
+        print 1
     else:
+        # One Edge in expansion
+        singleEdgeAns = (powTwo[v-2]*e)%mod
+        # Two Edges in expansion
+            # using 3 nodes
+        commonNodeEdgePairs = sum([(i*(i-1))/2 for i in edgeFrom[1:]])
+        commonNodeTwoEdgesAns = powTwo[v-3]*commonNodeEdgePairs
+            # using 4 nodes
+        noCommonNodeTwoEdgesAns = 0
+        if e>=4:
+            noCommonNodeEdgePairs = (e*(e-1))/2 - commonNodeEdgePairs
+            noCommonNodeTwoEdgesAns = powTwo[v-4]*noCommonNodeEdgePairs
+        # Three Edges in expansion
+            # using 3 nodes
+        ThreeEdgeUsingThreeNodes = 0
+        ThreeEdgeUsingThreeNodesAns = 0
+            # using 4 nodes
+        ThreeEdgeUsingFourNodes = 0
+        ThreeEdgeUsingFourNodesAns = 0
+            # using 5 nodes
+        ThreeEdgeUsingFiveNodes = 0
+        ThreeEdgeUsingFiveNodesAns = 0
+            # using 6 nodes
+        ThreeEdgeUsingSixNodes = 0
+        ThreeEdgeUsingSixNodesAns = 0
+
         if k == 1:
-            print (powTwo[v-2]*e)%mod
+            print singleEdgeAns
+        elif k == 2:
+            commonNodeTwoEdgesAns = (2*commonNodeTwoEdgesAns)%mod
+            noCommonNodeTwoEdgesAns = (2*noCommonNodeTwoEdgesAns)%mod
+            print (singleEdgeAns+commonNodeTwoEdgesAns+noCommonNodeTwoEdgesAns)%mod
+        elif k == 3 and v<=300:
+            setVariables3()
+            commonNodeTwoEdgesAns = (6*commonNodeTwoEdgesAns)%mod
+            noCommonNodeTwoEdgesAns = (6*noCommonNodeTwoEdgesAns)%mod
+            # print singleEdgeAns,commonNodeTwoEdgesAns,noCommonNodeTwoEdgesAns,ThreeEdgeUsingThreeNodesAns,ThreeEdgeUsingFourNodesAns,ThreeEdgeUsingFiveNodesAns,ThreeEdgeUsingSixNodesAns
+            print (singleEdgeAns+commonNodeTwoEdgesAns+noCommonNodeTwoEdgesAns+ThreeEdgeUsingThreeNodesAns+ThreeEdgeUsingFourNodesAns+ThreeEdgeUsingFiveNodesAns+ThreeEdgeUsingSixNodesAns)%mod
         else:
-            ans = 0
-            visited = [False for i in range(v+1)]
-            included = [False for i in range(v+1)]
-            dfs(1,0)
-            print ans
+            sys.exit()
